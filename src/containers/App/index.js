d0faf76550eb0e3ed45a6be133ba2c5e238c2180
Zakhar Kononov <zorgizorgik@gmail.com>
2019-09-14 20:01:36 +0300

    My initial setup

---
+++ src/containers/App/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+// import PropTypes from 'prop-types'
+// import { compose } from 'redux'
+// import { connect } from 'react-redux'
+
+class App extends PureComponent {
+  render () {
+    return <h1>Hello World to marsians</h1>
+  }
+}
+
+export default App
---
+++ webpack.config.babel.js
@@ -0,0 +1,113 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const url = require('postcss-url')
+const cssPresetEnv = require('postcss-preset-env')
+const atImport = require('postcss-import')
+
+const PATH = require('path')
+
+const { NODE_ENV } = process.env
+
+module.exports = {
+  entry: {
+    app: './src/index'
+  },
+  output: {
+    filename: 'chunk.[name].js?[hash]',
+    chunkFilename: 'chunk.[name].js?[chunkhash]',
+    path: PATH.resolve(__dirname, 'dist'),
+    publicPath: '/ants/'
+  },
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'initial'
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: PATH.resolve(__dirname, 'src'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localsConvention: 'camelCase',
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'by-ident',
+              plugins: () => [
+                atImport({
+                  path: './src'
+                }),
+                url({
+                  url: 'inline'
+                }),
+                cssPresetEnv
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          removeComments: false,
+          removeAttributeQuotes: false
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules', PATH.resolve(__dirname, 'src')]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(NODE_ENV)
+      }
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunks: ['runtime~app', 'vendors', 'app']
+    })
+  ],
+  ...(NODE_ENV === 'production' && {
+    devtool: 'source-map'
+  }),
+  ...(NODE_ENV === 'development' && {
+    devServer: {
+      port: 3080,
+      historyApiFallback: {
+        rewrites: [
+          {
+            from: /^\/$/,
+            to: '/ants/index.html'
+          }
+        ]
+      },
+      stats: {
+        colors: true
+      }
+    }
+  })
+}
