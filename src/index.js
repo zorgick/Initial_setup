4d5d379e546acbb4f74f401659e12f9479d1189a
Zackary Kononov <zorgik@Zackarys-MacBook-Pro.local>
2019-11-27 17:56:40 +0300

    Add polyfills for ie10+

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import 'core-js/es/promise' // Required for IE11 as webpack uses dynamic imports built on promises (import it explicitly in case of babel's useBuiltIns malfunction) [dynamic imports](https://webpack.js.org/guides/code-splitting/#dynamic-imports)
 
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -13,11 +14,32 @@ import './styles.css'
 
 const store = configureStore()
 
-ReactDOM.render(
-  <Router history={history}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>,
-  document.getElementById('root')
-)
+const render = () =>
+  ReactDOM.render(
+    <Router history={history}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>,
+    document.getElementById('root')
+  )
+
+// IE10+ check
+if (
+  'fetch' in window &&
+  'Intl' in window &&
+  'Map' in window &&
+  'Proxy' in window &&
+  'forEach' in window.NodeList.prototype &&
+  'startsWith' in String.prototype &&
+  'endsWith' in String.prototype &&
+  'includes' in String.prototype &&
+  'includes' in Array.prototype &&
+  'assign' in Object &&
+  'entries' in Object &&
+  'keys' in Object
+) {
+  render()
+} else {
+  import('utils/polyfills').then(render)
+}
---
+++ src/utils/polyfills.js
@@ -0,0 +1,4 @@
+require('isomorphic-fetch')
+require('intl')
+require('core-js/web/dom-collections')
+require('proxy-polyfill/src/proxy')
