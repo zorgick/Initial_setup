9014d656d92e4ce40476f1f602d2a9c577ad9be8
Zackary Kononov <zorgizorgik@gmail.com>
2019-12-30 16:14:16 +0300

    Remove error-prone plugin

--- babel.config.js
+++ babel.config.js
@@ -15,8 +15,7 @@ module.exports = {
     '@babel/plugin-transform-async-to-generator',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-syntax-dynamic-import',
-    "@loadable/babel-plugin"
+    '@babel/plugin-syntax-dynamic-import'
   ],
   env: {
     test: {
