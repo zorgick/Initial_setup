9b543b2eaaed5246c9bc42f43904ec0898eae26c
Zackary Kononov <zorgizorgik@gmail.com>
2020-01-09 15:15:29 +0300

    Add project documentation; brush up the code

--- src/shared/containers/MyComp/index.js
+++ src/shared/containers/MyComp/index.js
@@ -2,19 +2,24 @@ import * as React from 'react'
 import * as PropTypes from 'prop-types'
 import { createStructuredSelector } from 'reselect'
 import { compose } from 'redux'
+import { Helmet } from 'react-helmet-async'
+
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
+import withStyles from 'isomorphic-style-loader/withStyles'
 
 import injectReducer from 'shared/utils/injectReducer'
 import { selectSecondGreetings } from './selectors'
 import reducer from './reducer'
+import styles from './styles.css'
 
 export class MyComp extends React.PureComponent {
   render () {
     const { secondHi, t } = this.props
     return (
       <div>
+        <Helmet defaultTitle='My-Component | Ensemble' />
         {t(`common.${secondHi}`)}
         <Link to='/'>{t('common.back')}</Link>
       </div>
@@ -35,4 +40,9 @@ const withConnect = connect(mapStateToProps)
 const withLanguage = withTranslation('common')
 const withReducer = injectReducer({ key: 'my', reducer })
 
-export default compose(withReducer, withConnect, withLanguage)(MyComp)
+export default compose(
+  withStyles(styles),
+  withReducer,
+  withConnect,
+  withLanguage
+)(MyComp)
